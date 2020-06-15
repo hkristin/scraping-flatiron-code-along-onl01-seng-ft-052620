d4ec653f034bcc9ea0a008400660a523da029472
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-15 14:48:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,6 +12,7 @@ class Scraper
     doc.css(".post")
     doc.css("post").first.css("h2").text
     doc.css(".post").first.css(".date").text
+    doc.css(".post").first.css("p").text
   end
   
   def print_courses
